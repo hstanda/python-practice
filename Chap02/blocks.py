1f1817d6d3807d7dbd231d615edc04a7a101c237
Harjeevan <hstanda@gmail.com>
2018-09-22 16:10:48 -0400

    Chapter two complete

---
+++ Chap02/blocks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 42
+y = 73
+
+if x < y:
+    z = 112
+    print('x < y: x is {} and y is {}'.format(x, y))
+    print('line 2')
+    print('line 3')
+
+
+print('Z is {}'.format(z))
---
+++ Chap02/hello-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+import platform
+
+def main():
+    message()
+
+def message():
+    print('This is python version {}'.format(platform.python_version()))
+    print('line 2')
+    if False:                   
+        print('line 3')         #commets go here
+    else:
+        print('not true')       #comments by else 
+
+if __name__ == '__main__': main()
