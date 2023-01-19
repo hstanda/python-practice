cc75083777d201e7329136e8fee1f8101202f185
Harjeevan Tanda <harjeevan.tanda@gmail.com>
2023-01-19 15:31:26 -0500

    loops and data types

--- practice/practice.py
+++ practice/practice.py
@@ -31,7 +31,7 @@
 # 	print()
 
 
-#Program to print Left Half Pyramid
+# Program to print Left Half Pyramid
 # num_rows = int(input("Enter the number of rows"));
 # k = 1
 # for i in range(0, num_rows):
@@ -41,35 +41,89 @@
 #     print()
 
 
-num_rows = int(input("Enter the number of rows "))
-k = 0
-for i in range(1, num_rows + 1): 
-    for j in range (1, (num_rows - i) + 1): 
-        print(end = " ")          
-    while k != (2 * i - 1):
-        print("*", end = "")
+def print_stars(num_rows):
+    import time
+    if (num_rows > 15):
+        num_rows = 15
+    k = 0
+    for i in range(1, num_rows + 1):
+        time.sleep(1)
+        for j in range(1, (num_rows - i) + 1):
+            print(end=" ")
+        while k != (2 * i - 1):
+            print("*", end="")
+            k = k + 1
+        k = 0
+        print()
+
+    k = 2
+    m = 1
+    for i in range(1, num_rows):
+        time.sleep(1)
+        for j in range(1, k):
+            print(end=" ")
         k = k + 1
-    k = 0   
-    print()  
- 
-k = 2
-m = 1
-for i in range(1, num_rows): 
-    for j in range (1, k):
-        print(end = " ") 
-    k = k + 1	  
-    while m <= (2 * (num_rows - i) - 1): 
-        print("*", end = "") 
-        m = m + 1
-    m = 1	
-    print()
+        while m <= (2 * (num_rows - i) - 1):
+            print("*", end="")
+            m = m + 1
+        m = 1
+        print()
 
-# aaar = [12, 34, 25, 62, 12, 13, 37, 88, 44]
 
+# def insert_into_datasbe(num):
+#     import mysql.connector
+#     mydb = mysql.connector.connect(host="localhost", user="root", passwd="", database="practice")
+#     mycursor = mydb.cursor()
+#     statement = ("INSERT INTO customers (name, address) VALUES ('John ' + {}, 'Highway ' + {})".format(num, num))
+#     mycursor.execute(statement)
+#     mydb.commit()
+
+# data = [12, 34, 25, 62, 12, 13, 37, 88, 44]
 # smallest = None
-# for num in aaar:
-#     if(smallest == None):
+# for num in data:
+#     if (smallest == None):
 #         smallest = num
-#     elif(num < smallest):
+#     elif (num < smallest):
 #         smallest = num
+#     insert_into_datasbe(num)
+#     # print_stars(num)
+
 # print(smallest, 'smallest')
+
+
+counts = dict()
+names = ["Jack", "Michel", "Savrin", "Cherry", "Jack",
+         "Michel", "Savrin", "Jason", "Alia", "Savrin"]
+# for name in names:
+#     if name not in counts:
+#         counts[name] = 1
+#     else:
+#         counts[name] = counts[name]+1
+
+# for name in names:
+#     counts[name] = counts.get(name, 0) + 1
+# print(counts)
+
+# for key, value in counts.items():
+#     print(key, 'is ', value, 'times')
+
+
+# name = input("Enter the file name")
+# handle = open(name)
+handle = open("sample.txt")
+
+times = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        times[word] = times.get(word, 0)+1
+
+bigCount = None
+bigWord = None
+
+for word, count in times.items():
+    if bigCount is None or count > bigCount:
+        bigCount = count
+        bigWord = word
+
+print(bigWord,bigCount)
